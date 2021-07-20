51d60d27e42f33dae146a0d1ee6d2e80fca2101f
Ash Jones <74986794+DenseTurd@users.noreply.github.com>
2021-07-20 12:08:39 +0100

    Add files via upload

---
+++ RandoTation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class RandoTation : MonoBehaviour
+{ 
+    Vector3 from;
+    Vector3 to;
+    Vector3 toward;
+    float nextOrientationDelay;
+    float nextOrientationTime;
+    float step;
+    float slerpStep;
+    float slerpStepTime;
+    public float speed = 2f;
+
+    void Start()
+    {
+        SetNewOrientation();
+    }
+
+    void Update()
+    {
+        if (Time.time < nextOrientationTime)
+        {
+            toward = new Vector3(Mathf.Lerp(from.x, to.x, step), Mathf.Lerp(from.y, to.y, step), Mathf.Lerp(from.z, to.z, step));
+
+            transform.Rotate(toward.z, toward.x, toward.y, Space.World);
+            step += slerpStep * Time.deltaTime;
+        }
+        else
+        {
+            SetNewOrientation();
+        }
+    }
+
+    void SetNewOrientation()
+    {
+        from = to;
+        to = new Vector3(Random.Range(-speed, speed), Random.Range(-speed, speed), Random.Range(-speed, speed));
+        nextOrientationDelay = Random.Range(0.3f, 6f);
+        slerpStepTime = Random.Range(nextOrientationDelay, nextOrientationDelay * 1.2f);
+        slerpStep = 1 / slerpStepTime;
+        step = 0f;
+        nextOrientationTime = Time.time + nextOrientationDelay;
+    }
+}
